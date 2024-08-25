2cda2548a1baa3fdda296940418a948e24d17479
DiegoSalazarJara <diego.salazar2101@alumnos.ubiobio.cl>
2024-08-25 00:36:24 -0400

    ADD encryptPassword and comparePassword with bcryptjs

---
+++ backend/src/helpers/bcrypt.helper.js
@@ -0,0 +1,11 @@
+"use strict";
+import bcrypt from 'bcryptjs';
+
+export async function encryptPassword(password) {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+}
+
+export async function comparePassword(password, receivedPassword) {
+    return await bcrypt.compare(password, receivedPassword);
+}
