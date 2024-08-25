b790089a0c488e1fc20dd453c17e20e9462b5a31
DiegoSalazarJara <diego.salazar2101@alumnos.ubiobio.cl>
2024-08-25 00:27:33 -0400

    ADD config auth with passport.js

---
+++ backend/src/auth/passport.auth.js
@@ -0,0 +1,34 @@
+"use strict";
+import passport from 'passport';
+import User from '../entity/user.entity.js';
+import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
+import { ACCESS_TOKEN_SECRET } from '../config/configEnv.js';
+import { AppDataSource } from '../config/configDb.js';
+
+const options = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: ACCESS_TOKEN_SECRET
+};
+
+passport.use(new JwtStrategy(options, async (jwt_payload, done) => {
+    try {
+        const userRepository = AppDataSource.getRepository(User);
+        const user = await userRepository.findOne({ 
+                where: { 
+                    email: jwt_payload.email 
+                } 
+            });
+
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+        }
+    } catch (error) {
+        return done(error, false);
+    }
+}));
+
+export function passportJwtSetup() {
+    passport.initialize();
+}
