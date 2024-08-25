2da9eecc749e4dee90af1c74d63f6c6e10cc2fc8
DiegoSalazarJara <diego.salazar2101@alumnos.ubiobio.cl>
2024-08-25 00:33:40 -0400

    ADD handlers - Se agrega un handler para manejar error de cliente. - Se agrega un handler para manejar error de servidor. - Se agrega un handler para manejar la respuesta exitosa.

---
+++ backend/src/handlers/responseHandlers.js
@@ -0,0 +1,28 @@
+"use strict";
+
+export function handleSuccess(res, statusCode, message, data = {}) {
+    return res.status(statusCode)
+        .json({
+            status: "Success",
+            message,
+            data,
+        });
+}
+
+export function handleErrorClient(res, statusCode, message, details = {}) {
+    return res.status(statusCode)
+        .json({
+            status: "Client error",
+            message,
+            details,
+        });
+}
+
+export function handleErrorServer(res, statusCode, message, details = {}) {
+    return res.status(statusCode)
+        .json({
+            status: "Server error",
+            message,
+            details,
+        });
+}
