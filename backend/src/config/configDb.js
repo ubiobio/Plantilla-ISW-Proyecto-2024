a07970a93ef5f511dfca242af7f66a3366c6deee
DiegoSalazarJara <diego.salazar2101@alumnos.ubiobio.cl>
2024-08-25 00:27:56 -0400

    ADD config backend - Se agrega .env.example para dar a conocer las variables de entorno a utilizar. - Se agrega la configuración a la base de datos. - Se agrega la configuración de las variables de entorno. - Se agrega una configuración inicial al levantar el proyecto por primera vez.

---
+++ backend/src/config/configDb.js
@@ -0,0 +1,30 @@
+"use strict";
+import { DataSource } from 'typeorm';
+import { 
+    HOST, 
+    DB_USERNAME, 
+    PASSWORD, 
+    DATABASE
+} from './configEnv.js';
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: `${HOST}`,
+    port: 5432,
+    username: `${DB_USERNAME}`,
+    password: `${PASSWORD}`,
+    database: `${DATABASE}`,
+    entities: ["src/entity/**/*.js"],
+    synchronize: true,
+    logging: false
+});
+
+export async function connectDB() {
+    try {
+        await AppDataSource.initialize();
+        console.log('=> Conexión exitosa a la base de datos!');
+    } catch (error) {
+        console.error('Error al conectar con la base de datos:', error);
+        process.exit(1);
+    }
+}
---
+++ backend/src/config/configEnv.js
@@ -0,0 +1,20 @@
+"use strict";
+import { fileURLToPath } from "url";
+import path from "path";
+import dotenv from "dotenv";
+
+const _filename = fileURLToPath(import.meta.url);
+
+const _dirname = path.dirname(_filename);
+
+const envFilePath = path.resolve(_dirname, ".env");
+
+dotenv.config({ path: envFilePath });
+
+export const PORT = process.env.PORT;
+export const HOST = process.env.HOST;
+export const DB_USERNAME = process.env.DB_USERNAME;
+export const PASSWORD = process.env.PASSWORD;
+export const DATABASE = process.env.DATABASE;
+export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+export const cookieKey = process.env.cookieKey;
