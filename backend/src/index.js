88c90b62a01710aaa1c2d45a21e73a9ef08ec12a
DiegoSalazarJara <diego.salazar2101@alumnos.ubiobio.cl>
2024-08-25 00:37:54 -0400

    ADD index.js - Se agrega distintos middlewares para gestionar el proyecto. - Se levanta el servidor y conexión a la base de datos. - Se llama a la función createUsers para generar 2 usuarios por defecto.

---
+++ backend/src/index.js
@@ -0,0 +1,76 @@
+"use strict";
+import cors from "cors";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import indexRoutes from "./routes/index.routes.js";
+import session from 'express-session';
+import passport from "passport";
+import express, { urlencoded, json } from "express";
+import { PORT, HOST, cookieKey } from "./config/configEnv.js";
+import { connectDB } from "./config/configDb.js";
+import { createUsers } from "./config/initialSetup.js";
+import { passportJwtSetup } from "./auth/passport.auth.js";
+
+async function setupServer() {
+    try {
+        const app = express();
+
+        app.disable("x-powered-by");
+
+        app.use(cors({ 
+            credentials: true, 
+            origin: true 
+        }));
+
+        app.use(urlencoded({ 
+            extended: true,
+            limit: "1mb"
+        }));
+
+        app.use(json({
+            limit: "1mb"
+        }));
+
+        app.use(cookieParser());
+
+        app.use(morgan("dev"));
+
+        app.use(session({
+            secret: cookieKey,
+            resave: false,
+            saveUninitialized: false,
+            cookie: { 
+                secure: false,
+                httpOnly: true, 
+                sameSite: 'strict' 
+            }
+        }));
+
+        app.use(passport.initialize());
+        app.use(passport.session());
+
+        passportJwtSetup();
+
+        app.use("/api", indexRoutes);
+
+        app.listen(PORT, () => {
+            console.log(`=> Servidor corriendo en ${HOST}:${PORT}/api`);
+        });
+    } catch (error) {
+        console.log("Error en index.js -> setupServer(), el error es: ", error);
+    }
+}
+
+async function setupAPI() {
+    try {
+        await connectDB();
+        await setupServer();
+        await createUsers();
+    } catch (error) {
+        console.log("Error en index.js -> setupAPI(), el error es: ", error);
+    }
+}
+
+setupAPI()
+    .then(() => console.log("=> API Iniciada exitosamente"))
+    .catch((error) => console.log("Error en index.js -> setupAPI(), el error es: ", error));
