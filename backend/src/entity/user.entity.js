0decd7c6ac88ea04a94549faa790d5caa57fe12c
DiegoSalazarJara <diego.salazar2101@alumnos.ubiobio.cl>
2024-08-25 00:32:19 -0400

    ADD entity User - Se agrega la entidad de usuario. - Se define el nombre de la tabla y sus columnas.

---
+++ backend/src/entity/user.entity.js
@@ -0,0 +1,70 @@
+"use strict";
+import { EntitySchema } from 'typeorm';
+
+const UserSchema = new EntitySchema({
+    name: "User",
+    tableName: "users",
+    columns: {
+        id: {
+            type: "int",
+            primary: true,
+            generated: true
+        },
+        nombreCompleto: {
+            type: "varchar",
+            length: 255, 
+            nullable: false
+        },
+        rut: {
+            type: "varchar",
+            length: 12, 
+            nullable: false,
+            unique: true
+        },
+        email: {
+            type: "varchar",
+            length: 255, 
+            nullable: false,
+            unique: true
+        },
+        rol: {
+            type: "varchar",
+            length: 50, 
+            nullable: false
+        },
+        password: {
+            type: "varchar",
+            nullable: false,
+        },
+        createdAt: {
+            type: "timestamp with time zone",
+            default: () => "CURRENT_TIMESTAMP",
+            nullable: false
+        },
+        updatedAt: {
+            type: "timestamp with time zone",
+            default: () => "CURRENT_TIMESTAMP",
+            onUpdate: "CURRENT_TIMESTAMP",
+            nullable: false
+        }
+    },
+    indices: [
+        {
+            name: "IDX_USER",
+            columns: ["id"],
+            unique: true
+        },
+        {
+            name: "IDX_USER_RUT",
+            columns: ["rut"],
+            unique: true
+        },
+        {
+            name: "IDX_USER_EMAIL",
+            columns: ["email"],
+            unique: true
+        },
+    ],
+});
+
+export default UserSchema;
