6ca0b0e4b4c5ffce714aa00d1adbfff9a1d7f4fa
DiegoSalazarJara <diego.salazar2101@alumnos.ubiobio.cl>
2024-08-25 00:40:02 -0400

    ADD authorization and authentication middleware

---
+++ backend/src/middlewares/authentication.middleware.js
@@ -0,0 +1,20 @@
+"use strict";
+import passport from 'passport';
+
+export function authenticateJwt(req, res, next) {
+    passport.authenticate('jwt', { session: false }, (err, user, info) => {
+        if(err) {
+            return next(err);
+        }
+
+        if(!user) {
+            return res.status(401).json({
+            message: "No tienes permisos para acceder a este recurso"
+            });
+        }
+
+        req.user = user;
+        next();
+    
+    })(req, res, next);
+}
---
+++ backend/src/middlewares/authorization.middleware.js
@@ -0,0 +1,36 @@
+import User from '../entity/user.entity.js';
+import { AppDataSource } from '../config/configDb.js';
+import { 
+    handleErrorClient,
+    handleErrorServer } from '../handlers/responseHandlers.js';
+
+export async function isAdmin(req, res, next) {
+    try {
+        const userRepository = AppDataSource.getRepository(User);
+        
+        const userFound = await userRepository.findOneBy({ email: req.user.email });
+
+        if (!userFound) {
+            return handleErrorClient(
+                res, 
+                401, 
+                'Usuario no encontrado en la base de datos'
+            );
+        }
+
+        const rolUser = userFound.rol;
+
+        if (rolUser === 'administrador') {
+            next();
+            return;
+        }
+
+        return handleErrorClient(
+            res, 
+            401, 
+            'Se requiere un rol de administrador para realizar esta acción'
+        );
+    } catch (error) {
+        handleErrorServer(res, 500, 'Error en authorization.middleware -> isAdmin()', error.message);
+    }
+}
