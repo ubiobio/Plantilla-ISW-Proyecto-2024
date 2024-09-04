04b383ba218e7b085a7ee61037b79fd22c898849
DiegoSalazarJara <diego.salazar2101@alumnos.ubiobio.cl>
2024-09-04 04:04:03 -0400

    MOD style index.css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,44 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from '@pages/Login';
+import Home from '@pages/Home';
+import Users from '@pages/Users';
+import EditUser from '@pages/EditUser';
+import Register from '@pages/Register';
+import Error404 from '@pages/Error404';
+import Root from '@pages/Root';
+import '@styles/styles.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root/>,
+    errorElement: <Error404/>,
+    children: [
+      {
+        path: '/home',
+        element: <Home/>
+      },
+      {
+        path: '/users',
+        element: <Users/>
+      },
+      {
+        path: '/edit-user/:rut',
+        element: <EditUser/>
+      }
+    ]
+  },
+  {
+    path: '/auth',
+    element: <Login/>
+  },
+  {
+    path: '/register',
+    element: <Register/>
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router}/>
 )
