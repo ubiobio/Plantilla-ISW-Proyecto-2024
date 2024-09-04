8399fd41f2043af2dcc14a0c24641ed4b6db187e
DiegoSalazarJara <diego.salazar2101@alumnos.ubiobio.cl>
2024-09-04 04:12:43 -0400

    ADD lógica de hooks para obtener usuarios

---
+++ frontend/src/hooks/users/useGetUsers.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+import { getUsers } from '@services/user.service.js';
+
+const useUsers = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
+    const dataLogged = (formattedData) => {
+        try {
+            const { rut } = JSON.parse(sessionStorage.getItem('usuario'));
+            for(let i = 0; i < formattedData.length ; i++) {
+                if(formattedData[i].rut === rut) {
+                    formattedData.splice(i, 1);
+                    break;
+                }
+            }
+        } catch (error) {
+            console.error("Error: ", error)
+        }
+    }
+
+    const fetchUsers = async () => {
+    try {
+        const response = await getUsers();
+        const formattedData = response.map(user => ({
+            nombreCompleto: user.nombreCompleto,
+            rut: user.rut,
+            email: user.email,
+            rol: user.rol,
+            createdAt: user.createdAt
+        }));
+        dataLogged(formattedData);
+        setUsers(formattedData);
+    } catch (error) {
+        console.error("Error: ", error);
+    }
+}
+    return users;
+};
+
+export default useUsers;
