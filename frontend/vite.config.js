569b158928f4e7ac4b90692873d539758fded302
DiegoSalazarJara <diego.salazar2101@alumnos.ubiobio.cl>
2024-09-04 04:08:20 -0400

    ADD config vite: - Se agregan alias para las rutas de directorios. - Se agrega la configuración para producción.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-// https://vitejs.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {port:443, host:true},
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@context': path.resolve(__dirname, './src/context'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@helpers': path.resolve(__dirname, './src/helpers')
+    }
+  }
+});
