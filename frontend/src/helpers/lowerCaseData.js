61a9e95c161007578ea10881005322a3b2e9fc2d
DiegoSalazarJara <diego.salazar2101@alumnos.ubiobio.cl>
2024-10-06 05:44:58 -0300

    DEL assets, components and helpers

--- frontend/src/helpers/lowerCaseData.js
+++
@@ -1,8 +0,0 @@
-export function convertirMinusculas(obj) {
-    for (let key in obj) {
-        if (typeof obj[key] === 'string') {
-            obj[key] = obj[key].toLowerCase();
-        }
-    }
-    return obj;
-}
