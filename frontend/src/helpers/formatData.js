38ad3a874dc27032647e8199b4043c1c6ca9b0a7
DiegoSalazarJara <diego.salazar2101@alumnos.ubiobio.cl>
2024-10-06 05:47:48 -0300

    ADD alerts and data format

--- frontend/src/helpers/formatData.js
+++ frontend/src/helpers/formatData.js
@@ -1,4 +1,4 @@
-import startCase from 'lodash/startCase';
+import { startCase } from 'lodash';
 import { format as formatRut } from 'rut.js';
 import { format as formatTempo } from "@formkit/tempo";
 
@@ -8,6 +8,25 @@ export function formatUserData(user) {
         nombreCompleto: startCase(user.nombreCompleto),
         rol: startCase(user.rol),
         rut: formatRut(user.rut),
+        createdAt: formatTempo(user.createdAt, "DD-MM-YYYY")
+    };
+}
+
+export function convertirMinusculas(obj) {
+    for (let key in obj) {
+        if (typeof obj[key] === 'string') {
+            obj[key] = obj[key].toLowerCase();
+        }
+    }
+    return obj;
+}
+
+export function formatPostUpdate(user) {
+    return {
+        nombreCompleto: startCase(user.nombreCompleto),
+        rol: startCase(user.rol),
+        rut: formatRut(user.rut),
+        email: user.email,
         createdAt: formatTempo(user.createdAt, "DD-MM-YYYY")
     };
 }
--- frontend/src/helpers/sweetAlert.js
+++ frontend/src/helpers/sweetAlert.js
@@ -1,30 +1,7 @@
 import Swal from 'sweetalert2';
 
-const toastTypes = {
-  200: "success",
-  201: "success",
-  400: "warning",
-  404: "warning",
-  500: "error",
-};
-
-export function sweetAlert(status, message) {
-
-    const toastFunction = toastTypes[status] || "error";
-
-    Swal.fire({
-      position: "top-end",
-      icon: toastFunction,
-      text: message,
-      showConfirmButton: false,
-      width: '26em',
-      timer: 3000
-    });
-}
-
-export function deleteDataAlert(status, message) {
-  const toastFunction = toastTypes[status] || "error";
-  Swal.fire({
+export async function deleteDataAlert() {
+  return Swal.fire({
     title: "¿Estás seguro?",
     text: "¡No podrás revertir esto!",
     icon: "warning",
@@ -32,13 +9,21 @@ export function deleteDataAlert(status, message) {
     confirmButtonColor: "#3085d6",
     cancelButtonColor: "#d33",
     confirmButtonText: "Si, eliminar!"
-  }).then((result) => {
-    if (result.isConfirmed) {
-      Swal.fire({
-        title: "Eliminado!",
-        text: message,
-        icon: toastFunction
-      });
-    }
-  });
+  })
 }
+
+export const showSuccessAlert = (titleMessage, message) => {
+  Swal.fire(
+    titleMessage,
+    message,
+    'success'
+  );
+};
+
+export const showErrorAlert = (titleMessage, message) => {
+  Swal.fire(
+    titleMessage,
+    message,
+    'error'
+  );
+};
